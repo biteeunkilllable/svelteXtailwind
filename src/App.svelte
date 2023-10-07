209335810fd415de9073f6b028fde00ff80d9a6e
biteeunkilllable <90487296+biteeunkilllable@users.noreply.github.com>
2023-10-07 08:29:56 -0700

    init

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import TailwindCss from './lib/tailwindCss.svelte';
+</script>
+<main>
+  <h1 class="text-6xl text-violet-900 italic">Hola Mundo</h1>
+</main>
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./**/*.{svelte,html,js,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
